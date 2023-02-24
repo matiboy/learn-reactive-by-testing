b6c876787e016bb85ab409a3ce631e189170c535
Mat <mathieu@redapesolutions.com>
2023-02-24 10:56:36 +0800

    Serial disposable

--- learn_reactive_by_testing/disposables/test_serialdisposable.py
+++ learn_reactive_by_testing/disposables/test_serialdisposable.py
@@ -16,15 +16,25 @@ def test_serial():
     scheduler = TestScheduler()
     xs = scheduler.create_cold_observable(
         *[on_next(10+i*100, i) for i in range(10)],
-        on_completed(800)
+        on_completed(380)
     )
     sub = disposable.SerialDisposable()
     observer_1 = scheduler.create_observer()
-    observer_2 = scheduler.create_observer()
     sub.set_disposable(xs.subscribe(observer_1))
+    scheduler.schedule_absolute(250, lambda *_: sub.set_disposable(xs.subscribe(observer_1)))
     
     scheduler.start()
     assert observer_1.messages == [
-        on_next(800, True),
-        on_completed(800)
+        on_next(10, 0),
+        on_next(110, 1),
+        on_next(210, 2),
+        on_next(260, 0),
+        on_next(360, 1),
+        on_next(460, 2),
+        on_next(560, 3),
+        on_completed(630),
+    ]
+    assert xs.subscriptions == [
+        subscribe(0,250),
+        subscribe(250, 630)
     ]
